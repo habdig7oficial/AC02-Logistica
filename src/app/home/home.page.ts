b095a974e7abf6f73e2c5bd02d01028f92eea85c
Mateus Felipe Vieira <habdig7@gmail.com>
2023-03-21 11:41:23 +0000

    v0.0.2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 
+import { AlertController } from '@ionic/angular';
+
+import TabelaTempo from './tabela.tempo';
+
+interface IonSel extends Event {
+  detail?: {
+      value: string;
+  };
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  tipo_envio: IonSel | undefined;
+  tempo: number = 0 
 
-  constructor() {}
+  constructor(public allertController: AlertController) {
+
+  }
+
+  Calcular(){
+    
+   }
+
+   async exibirEnvio(){
+
+    this.tipo_envio?.detail?.value == 'Comum' ? this.tempo = TabelaTempo.Comum: null
+    this.tipo_envio?.detail?.value == 'Expresso' ? this.tempo = TabelaTempo.Expresso: null
+    this.tipo_envio?.detail?.value == 'Same Day' ? this.tempo = TabelaTempo.SameDay: null
+    this.tipo_envio?.detail?.value == 'Fornecedor' ? this.tempo = TabelaTempo.SameDay: null
+
+
+    const alerta = await this.allertController.create({
+      header:  `Tipo de Envio ${this.tipo_envio?.detail?.value}`,
+      message: `Seu pedido chegará em até ${this.tempo} horas`,
+    })
+    await alerta.present()
+   }
 
 }
---
+++ src/app/home/tabela.tempo.ts
@@ -0,0 +1,18 @@
+interface Tempo_Entrega {
+    Comum: number,
+    Expresso: number,
+    SameDay: number
+    Fornecedor: number
+}
+
+
+/* Tempo em  horas */
+
+let tabela_tempo: Tempo_Entrega = {
+    Comum: 120,
+    Expresso: 48,
+    SameDay: 12,
+    Fornecedor: 240
+}
+
+export default tabela_tempo;
