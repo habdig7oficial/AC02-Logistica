5428ddd148de22196b2e691a50e5cda2774046a3
Mateus Felipe Vieira <habdig7@gmail.com>
2023-03-27 17:59:31 +0000

    v0.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { AlertController } from '@ionic/angular';
 
-import TabelaTempo from './tabela.tempo';
+import { TabelaTempo, TabelaPreco } from './tabela.tempo';
 
 interface IonSel extends Event {
   detail?: {
@@ -19,19 +19,19 @@ export class HomePage {
   
   tipo_envio: IonSel | undefined;
   tempo: number = 0 
+  tempo_final: number = this.tempo
+  valor_final?: number;
 
   constructor(public allertController: AlertController) {
 
   }
 
-  Calcular(){
-    
-   }
+
 
    async exibirEnvio(){
 
     this.tipo_envio?.detail?.value == 'Comum' ? this.tempo = TabelaTempo.Comum: null
-    this.tipo_envio?.detail?.value == 'Expresso' ? this.tempo = TabelaTempo.Expresso: null
+    this.tipo_envio?.detail?.value == 'Expressa' ? this.tempo = TabelaTempo.Expresso: null
     this.tipo_envio?.detail?.value == 'Same Day' ? this.tempo = TabelaTempo.SameDay: null
     this.tipo_envio?.detail?.value == 'Fornecedor' ? this.tempo = TabelaTempo.SameDay: null
 
@@ -39,8 +39,14 @@ export class HomePage {
     const alerta = await this.allertController.create({
       header:  `Tipo de Envio ${this.tipo_envio?.detail?.value}`,
       message: `Seu pedido chegará em até ${this.tempo} horas`,
+      buttons:[`OK`]
     })
     await alerta.present()
+   }
+
+   async Calcular(){
+      this.valor_final = 1
+      return this.valor_final;
    }
 
 }
--- src/app/home/tabela.tempo.ts
+++ src/app/home/tabela.tempo.ts
@@ -1,18 +1,30 @@
 interface Tempo_Entrega {
-    Comum: number,
-    Expresso: number,
-    SameDay: number
-    Fornecedor: number
+  Comum: number;
+  Expresso: number;
+  SameDay: number;
+  Fornecedor: number;
 }
 
+interface Preco_Entrega {
+  Comum: { comissao: number; multiplicador: number };
+  Expresso: { comissao: number; multiplicador: number };
+  SameDay: { comissao: number; multiplicador: number };
+  Fornecedor: { comissao: number; multiplicador: number };
+}
 
 /* Tempo em  horas */
 
-let tabela_tempo: Tempo_Entrega = {
-    Comum: 120,
-    Expresso: 48,
-    SameDay: 12,
-    Fornecedor: 240
-}
+export let TabelaTempo: Tempo_Entrega = {
+  Comum: 120,
+  Expresso: 48,
+  SameDay: 12,
+  Fornecedor: 240,
+};
 
-export default tabela_tempo;
+/* Commisao Em Reais, Multiplicar Distância */
+export let TabelaPreco: Preco_Entrega = {
+  Comum: { comissao: 0, multiplicador: 1 },
+  Expresso: { comissao: 10, multiplicador: 1.2 },
+  SameDay: { comissao: 12.99, multiplicador: 1.4 },
+  Fornecedor: { comissao: 0, multiplicador: 0.7 },
+};
